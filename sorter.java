092f1dc280206088dde598a1806203c1449c964e
sultanabbas0 <sultanabbas018@gmail.com>
2023-09-19 15:33:41 +0300

    computer b, implemented insertionsort

--- sorter.java
+++ sorter.java
@@ -20,12 +20,47 @@ public class sorter {
     }
 
     public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};
+        int[] arr = { 64, 25, 12, 22, 11 };
         selectionSort(arr);
 
         System.out.println("Sorted array:");
         for (int i : arr) {
             System.out.print(i + " ");
         }
+    }
+}
+
+class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 8, 3, 1 };
+        System.out.println("Array before sorting:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Array after sorting:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
